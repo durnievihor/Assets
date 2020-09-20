fada41ddbfc1176fc401e2288b42ca35744d2633
Ihor Durniev <31383299+durnievihor@users.noreply.github.com>
2020-09-20 20:29:21 +0300

    Initial commit

---
+++ Scripts/ParticlePlay.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticlePlay : MonoBehaviour
+{
+
+    public ParticleSystem ParSys;
+    public bool played = false;
+
+    void Awake()
+    {
+        ParSys = GetComponent<ParticleSystem>();
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        ParSys.Play();
+        played = true;
+    }
+
+    public void Update()
+    {
+        if (ParSys && played && !ParSys.IsAlive())
+        {             
+            Destroy(gameObject);
+        }
+    }
+}
